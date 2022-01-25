2d753b047860ebd6ce1277d1d7cc44d8831c6149
Misstery13 <misstery13@outlook.com>
2022-01-25 18:54:18 -0500

    Diana was here

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	// write your code here
         System. out. println("Hello, my name is Laura");
         System.out.println("Hello Laura, my name is Dhener.");
+        System.out.println("Hello Laura, my name is Diana");
     }
 
 
